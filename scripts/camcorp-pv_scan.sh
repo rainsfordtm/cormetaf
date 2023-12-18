d2550ddfad9ab4e64197ebcf305f4a3089ecc661
Tom Rainsford <tmr740-ac@yahoo.co.uk>
2023-12-18 19:13:19 +0100

    Rescanned camcorp-v and camcorp-pv, corrected unstressed categories

--- scripts/camcorp-pv_scan.sh
+++ scripts/camcorp-pv_scan.sh
@@ -2,7 +2,7 @@
 
 #Global variables
 scan_file="$HOME/git/syllabic-verse-analysis-code/scan_text.py"
-corpdir="$HOME/Corpus/corpora/camcorp-pv"
+corpdir="$HOME/git/camcorp/camcorp-pv"
 phonemes="${corpdir}/cfg/cv-phonemes.cfg"
 unstressed="${corpdir}/cfg/unstressed_major_cats.txt"
 
